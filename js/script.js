3774bad178a78c819384955fbb7636245c57a01a
Jan Choma <jandominikchoma@gmail.com>
2018-04-12 16:22:50 +0200

    inView without exit and scrollspy nav

--- js/script.js
+++ js/script.js
@@ -1,15 +1,6 @@
-//
-//
-// var inViewVariable = inView.is(document.querySelector('.team-member'))
-//
-// if (inViewVariable){
-//     var teamMember = document.querySelector(".team-member")
-//     teamMember.style.border = "10px solid black"
-// }
-
-
 inView('.team-member')
     .on('enter', el =>
-el.classList.add('in-view'))
-.on('exit', el =>
-el.classList.remove('in-view'));
+        el.classList.add('in-view'))
+
+
+
