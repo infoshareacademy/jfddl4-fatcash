e88b179967961cb005dee30af291d6b670f1f3aa
vizzroot <vizzroot@gmail.com>
2018-04-13 15:43:28 +0200

    ciastka

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$().ready(function() {
+    var sName = "cookiesok";
+    $("#close-cookie-warn").click(function(){
+        var oExpire = new Date();
+        oExpire.setTime((new Date()).getTime() + 3600000*24*365);
+        document.cookie = sName + "=1;expires=" + oExpire;
+        $("#cookie-warn").hide("slow");
+    });
+
+
+
+    var sStr = '; '+ document.cookie +';';
+    var nIndex = sStr.indexOf('; '+ escape(sName) +'=');
+    if (nIndex === -1) {
+        $("#cookie-warn").show();
+    }
+});
